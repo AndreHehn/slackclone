2fdc6a1b05fc7d505f68f1e47672579250e970e2
Andre Hehn <a.hehn@web.de>
2022-08-02 17:06:59 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChatboxComponent } from './chatbox/chatbox.component';
+import { ChannelListComponent } from './channel-list/channel-list.component';
+import { ChannelComponent } from './channel/channel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatboxComponent,
+    ChannelListComponent,
+    ChannelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
