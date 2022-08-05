c7b44f299dee29f7b62279450c69b034ad04cfc2
JuergenHildbrand <jhildbrand@pm.me>
2022-08-05 15:57:37 +0200

    css login-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './shared/services/auth.service';
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,7 @@ import { AuthService } from './shared/services/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   title = 'slackclone';
   connected = false;
   showSearch = false;
@@ -16,7 +19,7 @@ export class AppComponent {
     public authService: AuthService) { }
 
 
- public ngOnInit() {
+  public ngOnInit() {
     this.connected = (this.authService.isLoggedIn) ? true : false;
   }
 
