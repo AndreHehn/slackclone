6798586eff4df5db32f244ce2b8c3cbfb0b7639f
Andre Hehn <a.hehn@web.de>
2022-08-05 19:57:01 +0200

    changed chatbox to quill input / value is binded to var

--- src/app/chatbox/chatbox.component.ts
+++ src/app/chatbox/chatbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
 
 @Component({
   selector: 'app-chatbox',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatboxComponent implements OnInit {
 
+  modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      ['link', 'image', 'video']                         // link and image, video
+    ]
+  };
+
+  message = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  print() {
+    console.log(this.message);
+    // write code to save it in firestore later.
+  }
+
+  changedEditor(event: EditorChangeContent | EditorChangeSelection) {
+    this.message = event['editor']['root']['innerHTML'];
+  }
 }
