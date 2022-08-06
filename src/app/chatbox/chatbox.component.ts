b29e1fb9e2b1084e759b4abd263062d191a5a34b
Andre Hehn <a.hehn@web.de>
2022-08-06 11:39:21 +0200

    seperated picture upload from text / picture saved directly in storage

--- src/app/chatbox/chatbox.component.ts
+++ src/app/chatbox/chatbox.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
 
 @Component({
@@ -13,20 +17,73 @@ export class ChatboxComponent implements OnInit {
       ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
       ['blockquote', 'code-block'],
       [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      ['link', 'image', 'video']                         // link and image, video
+      ['link']                         // link and image, video
     ]
   };
+  preview: string;
+  message: string = '';
+  currentFile = null;
+  loading: boolean = false;
+  uploaded: boolean = false;
+  uploadPercent: Observable<number>;
+  saved: boolean = false;
 
-  message = '';
-  constructor() { }
+
+  constructor(private firestore: AngularFirestore,
+    private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
   }
 
-  print() {
+  ngOnDestroy(): void {
+    if (this.uploaded && !this.saved) { this.deleteLastUpload(); }
+  }
+
+  /**
+   * deletes the last Upload to save space at backend.
+   */
+  deleteLastUpload() {
+    if (this.currentFile !== null) {
+      const storageRef = this.currentFile;
+      storageRef.delete();
+      this.preview = '';
+      this.currentFile = null;
+    }
+  }
+
+  /**
+   * 
+   * Uploads picture to the fire storage.
+   */
+  uploadFile(event) {
+    this.loading = true;
+    const file = event.target.files[0];
+    const filePath = 'message_images' + Math.floor(Math.random() * 1000000000);
+    const fileRef = this.storage.ref(filePath);
+    this.currentFile = fileRef;
+    const task = this.storage.upload(filePath, file);
+    this.uploadPercent = task.percentageChanges();    // observe percentage changes
+    task.snapshotChanges().pipe(
+      finalize(() => this.saveUrl(fileRef))
+    ).subscribe();
+  }
+
+  /**
+   *  saves url of image to a variable
+   *
+   */
+  saveUrl(fileRef) {
+    fileRef.getDownloadURL().subscribe(url => { this.preview = url; });
+    this.loading = false;
+    this.uploaded = true;
+  }
+
+
+  saveAndSend() {
     console.log(this.message);
     // write code to save it in firestore later.
   }
+
 
   changedEditor(event: EditorChangeContent | EditorChangeSelection) {
     this.message = event['editor']['root']['innerHTML'];
