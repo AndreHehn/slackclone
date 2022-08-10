933a3068bf73830e9e529a63f5f2ab2a37dc83cc
Andre Hehn <a.hehn@web.de>
2022-08-10 19:29:46 +0200

    worked on dialog channel info

--- src/app/dialog-channel-info/dialog-channel-info.component.ts
+++ src/app/dialog-channel-info/dialog-channel-info.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogChannelInfoComponent implements OnInit {
 
+  countMembers: number = null;
+  userList;
+  creator;
   constructor() { }
 
   ngOnInit(): void {
